c6bf7e83819f06b2a2c2043919caa16898be33c8
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-30 23:53:43 -0700

    pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import { useRef, useState } from "react";
+
 function App(){
+
+    const InputValue = useRef()
+    const [arr,setArr] = useState([])
+
+    const FormValue = (e)=>{
+        e.preventDefault()
+        arr.push(InputValue.current.value)
+        console.log(arr);
+        setArr([...arr])
+        
+
+
+
+        InputValue.current.value = "";
+        
+    }
     return (
-        <h1>hello world</h1>
+        <>
+        <h1>hello Todo</h1>
+        <form onSubmit={FormValue}>
+            <input type="text" placeholder="Add a todo..." ref={InputValue} />
+            <button type="submit">Add</button>
+        </form>
+
+        <div>
+            {arr.map((item,index)=>{
+                return <div key={index}>
+                    <p>{item}</p>
+                    <button>DELETE</button>
+                    <button>EDIT</button>
+                </div>
+            })}
+        </div>
+        </>
     )
 }
 
