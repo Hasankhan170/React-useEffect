dcebddf6fc420620b686545fe22a0b9fd227a30c
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-31 05:34:32 -0700

    axios download

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 
 
 function App(){
+
+    useEffect(()=>{
+        console.log('hello world');
+        
+    },[])
     return (
         <>
         <h1>Hello World</h1>
