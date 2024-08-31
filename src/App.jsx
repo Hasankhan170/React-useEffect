babc6d3dfccd540a4b84e356e96ad5405a8b5dd9
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-31 00:12:35 -0700

    "Removed todo list functionality and replaced with 'Hello World' and a Button component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
-import { useRef, useState } from "react";
+
+import Button from './components/Button';
 
 function App(){
-
-    const InputValue = useRef()
-    const [arr,setArr] = useState([])
-
-    const FormValue = (e)=>{
-        e.preventDefault()
-        arr.push(InputValue.current.value)
-        console.log(arr);
-        setArr([...arr])
-        
-
-
-
-        InputValue.current.value = "";
-        
-    }
-
-    const DeleteTodo = (index)=>{
-        arr.splice(index,1);
-        setArr([...arr]);
-    }
-    const EditTodo = (index)=>{
-        const NewValue = prompt('New value')
-        arr.splice(index,1 ,NewValue)
-        setArr([...arr]);
-    }
     return (
         <>
-        <h1>hello Todo</h1>
-        <form onSubmit={FormValue}>
-            <input type="text" placeholder="Add a todo..." ref={InputValue} />
-            <button type="submit">Add</button>
-        </form>
-
-        <div>
-            {arr.map((item,index)=>{
-                return <div key={index}>
-                    <p>{item}</p>
-                    <button onClick={()=> DeleteTodo(index)}>DELETE</button>
-                    <button onClick={()=>EditTodo(index)}>EDIT</button>
-                </div>
-            })}
-        </div>
+        <h1>Hello World</h1>
+        <Button />
         </>
     )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button(){
+    return (
+        <>
+        <h1>Todo List</h1>
+        </>
+    )
+}
+
+export default Button;
