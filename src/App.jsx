f68a7cadaa70db10ffdf4e74299aaabf81c86537
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-31 06:09:41 -0700

    api handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 function App(){
 
+    const [arr,setArr] = useState(null)
+
     useEffect(()=>{
-        console.log('hello world');
+       async function getData(){
+            const res = await  axios('https://jsonplaceholder.typicode.com/users');
+            console.log(res.data);
+            setArr(res.data)
+            
+        }
+        getData()
         
     },[])
     return (
         <>
         <h1>Hello World</h1>
+
+    {arr ? arr.map((item)=>{
+                return <div key={item.id}>
+                    <p>{item.name}</p>
+                </div>
+     }):
+     <p>Loading...</p>
+  }
 
         </>
     )
